c56a4033c720574fd0e29aca92187e82c387f7b9
Shotaro Tao <taoshotaro@gmail.com>
2020-01-17 18:40:52 +0900

    Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VueFlux",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9),
+        .macOS(.v10_10),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "VueFlux",
+            targets: ["VueFlux"]
+        ),
+        .library(
+            name: "VueFluxReactive",
+            targets: ["VueFluxReactive"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "VueFlux",
+            path: "VueFlux",
+            sources: ["*", "../VueFluxInternalCore"]
+        ),
+        .target(
+            name: "VueFluxReactive",
+            dependencies: ["VueFlux"],
+            path: "VueFluxReactive"
+        ),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["VueFlux", "VueFluxReactive"],
+            path: "Tests"
+        )
+    ],
+    swiftLanguageVersions: [.v4_2]
+)
